9bef73bfa341c7af83d474f28193ae0da346ac5f
Ryan Grey <rgrey@scottlogic.co.uk>
2017-06-28 12:57:35 +0100

    Alter data to make tick label appear at very end of chart

--- src/service/DataService.js
+++ src/service/DataService.js
@@ -3,20 +3,17 @@ import { utc } from 'moment';
 export class DataService {
 
     static getData() {
-        const now = utc();
-        const minutesTruncatedToHalfHour = now.minute() >= 30 ? 30 : 0;
-        const fromDate = utc(now)
-            .minute(minutesTruncatedToHalfHour)
-            .second(0)
-            .millisecond(0)
-            .subtract(1, 'day')
-            .subtract(30, 'minute');
+        const minuteIncrement = 60;
+        const fromDate = utc().startOf('day')
+            .subtract(minuteIncrement, 'minute');
 
-        return DataService.baseData().map((value) => ({
-                date: fromDate.add(30, 'minute').clone(),
+        const data = DataService.baseData().map((value) => ({
+                date: fromDate.add(minuteIncrement, 'minute').clone(),
                 value
             })
         );
+
+        return data;
     }
 
     static baseData() {
@@ -24,9 +21,6 @@ export class DataService {
             0.20, 0.20, 0.21, 0.22, 0.23, 0.22, 0.22, 0.23,
             0.26, 0.30, 0.35, 0.42, 0.50, 0.60, 0.78, 0.96,
             1.00, 0.98, 0.99, 0.97, 1.00, 1.00, 0.98, 0.99,
-            0.97, 0.99, 0.99, 0.98, 1.00, 1.00, 1.00, 0.98,
-            0.97, 0.96, 0.96, 0.97, 0.99, 1.00, 1.00, 0.98,
-            0.99, 0.95, 0.86, 0.74, 0.56, 0.39, 0.25, 0.21
         ];
     }
 }
